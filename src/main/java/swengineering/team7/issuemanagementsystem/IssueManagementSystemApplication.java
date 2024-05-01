9fc7756a374d65f46f886b72cb58bf8fade9a56a
sinseongseop <sss02030@naver.com>
2024-05-01 19:17:35 +0900

    FEAT: 초기 프로젝트 틀(스프링부트3) 설정

---
+++ src/main/java/swengineering/team7/issuemanagementsystem/IssueManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package swengineering.team7.issuemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swengineering/team7/issuemanagementsystem/IssueManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package swengineering.team7.issuemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
