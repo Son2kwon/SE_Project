2d2b5bc8a21dbe481b130fed0bbea1f5e36fdf45
sinseongseop <sss02030@naver.com>
2024-05-11 17:09:59 +0900

    FiX: projectRepository 상속 오류 수정
    1. 상속 대상 오류 수정 (Comment -> Project)

--- src/main/java/swengineering/team7/issuemanagementsystem/repository/ProjectRepository.java
+++ src/main/java/swengineering/team7/issuemanagementsystem/repository/ProjectRepository.java
@@ -2,7 +2,8 @@ package swengineering.team7.issuemanagementsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import swengineering.team7.issuemanagementsystem.entitiy.Comment;
+import swengineering.team7.issuemanagementsystem.entitiy.Project;
 
-public interface ProjectRepository extends JpaRepository<Comment, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long> {
     // 기본 CRUD 제공
 }
