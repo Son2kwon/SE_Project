f2dd993eec2ef6853047a0a1a3af33f301cae9fd
sinseongseop <sss02030@naver.com>
2024-05-11 15:09:00 +0900

    FEAT: issueService- CRUD 개발
    1. issue와 관련된 CRUD 기능 개발

--- src/main/java/swengineering/team7/issuemanagementsystem/repository/IssueRepository.java
+++ src/main/java/swengineering/team7/issuemanagementsystem/repository/IssueRepository.java
@@ -3,6 +3,20 @@ package swengineering.team7.issuemanagementsystem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import swengineering.team7.issuemanagementsystem.entitiy.Issue;
 
+import java.util.List;
+
 public interface IssueRepository extends JpaRepository<Issue, Long> {
     // 기본 CRUD 제공
+
+    //ID 기준 정렬
+    List<Issue> findByTitleContaining(String keyword);
+    List<Issue> findByStateContaining(String state);
+    List<Issue> findByReporter_NameContaining(String name);
+
+    //최근순 정렬
+    List<Issue> findByTitleContainingOrderByDateDesc(String keyword);
+    List<Issue> findByStateContainingOrderByDateDesc(String state);
+    List<Issue> findByReporter_NameContainingOrderByDateDesc(String partialUserName);
+
 }
+
