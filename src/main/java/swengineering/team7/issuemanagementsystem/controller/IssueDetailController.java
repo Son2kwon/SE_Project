2027fe2dc8b162e54807028b343b4347e633d7d5
sinseongseop <sss02030@naver.com>
2024-05-18 19:10:19 +0900

    refactor: 변수명 정리

--- src/main/java/swengineering/team7/issuemanagementsystem/controller/IssueDetailController.java
+++ src/main/java/swengineering/team7/issuemanagementsystem/controller/IssueDetailController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import swengineering.team7.issuemanagementsystem.DTO.IssueDTO;
-import swengineering.team7.issuemanagementsystem.entity.Issue;
 import swengineering.team7.issuemanagementsystem.service.IssueService;
 
 import java.util.List;
