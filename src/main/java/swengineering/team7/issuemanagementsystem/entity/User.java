ddac113edfe5edf071a3432e3d284b4ed9c1a413
noir-01 <smu08054@gmail.com>
2024-05-25 02:14:28 +0900

    FIx: 초기 Role = null

--- src/main/java/swengineering/team7/issuemanagementsystem/entity/User.java
+++ src/main/java/swengineering/team7/issuemanagementsystem/entity/User.java
@@ -22,8 +22,6 @@ public class User {
         this.username=username;
         this.password=password;
         this.Contract=contract;
-        //test: 일단은 admin
-        this.role = "admin";
     }
     // User:Project 다:다     두 엔터티 연결하는 중간 테이블 생성
     @ManyToMany
@@ -45,8 +43,6 @@ public class User {
     // User:Comment 1:다
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Comment> comments;
-
-
 
     //Getter & Setter
     public String getId() {
