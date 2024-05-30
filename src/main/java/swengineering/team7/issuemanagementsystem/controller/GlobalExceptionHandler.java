d7f8e61d5914fdf37a5ab19f1ef8b7b53e0747a1
sinseongseop <sss02030@naver.com>
2024-05-30 15:50:27 +0900

    Feat: 예외처리 코드 분리(ProjectNotFoundException, UserNotFoundException)

--- src/main/java/swengineering/team7/issuemanagementsystem/controller/GlobalExceptionHandler.java
+++ src/main/java/swengineering/team7/issuemanagementsystem/controller/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import swengineering.team7.issuemanagementsystem.exception.ProjectNotFoundException;
+import swengineering.team7.issuemanagementsystem.exception.UserNotFoundException;
 import swengineering.team7.issuemanagementsystem.exception.WrongPriorityException;
 
 @ControllerAdvice
@@ -14,5 +16,14 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(ProjectNotFoundException.class)
+    public ResponseEntity<?> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(UserNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
 
 }
